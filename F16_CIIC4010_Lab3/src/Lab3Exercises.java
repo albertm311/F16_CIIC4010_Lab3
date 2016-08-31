ca2bd67474321efd13907ce567c3f2af19526633
Albert Morales <albert.morales@upr.edu>
2016-08-31 00:23:51 -0400

    Change the title, size and location of the popup window (JFrame).

--- F16_CIIC4010_Lab3/src/Lab3Exercises.java
+++ F16_CIIC4010_Lab3/src/Lab3Exercises.java
@@ -2,10 +2,11 @@ import javax.swing.JFrame;
 
 public class Lab3Exercises {
 	public static void main(String[] args) {
-		JFrame myFrame = new JFrame("Hello, world!");
-		//myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(200, 200);
-		myFrame.setSize(400, 100);
+		JFrame myFrame = new JFrame();
+		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		myFrame.setTitle("Albert");
+		myFrame.setLocation(550, 250);
+		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
 }
